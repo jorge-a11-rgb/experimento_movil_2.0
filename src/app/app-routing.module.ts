c250ca4ed0e7eba0959784ff660823ef9fd358d9
Jorge Alfaro Castro <jorgeandresalfarocastro@gmail.com>
2021-08-28 13:42:22 -0400

    fase 1 lista

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,30 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'contenido',
+    loadChildren: () => import('./pages/contenido/contenido.module').then( m => m.ContenidoPageModule)
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
   },
 ];
 
---
+++ src/app/model/NivelEducacional.ts
@@ -0,0 +1,9 @@
+export class NivelEducacional {
+  public id: number;
+  public nombre: string;
+
+  constructor(id: number, nombre: string) {
+    this.id = id;
+    this.nombre = nombre;
+  }
+}
